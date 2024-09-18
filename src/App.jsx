fe4d9a10d25582adfb88256a724aad796ab40b8a
sahidDev09 <iamsahidofficial99@gmail.com>
2024-09-19 01:53:41 +0600

    abid added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Testing from "./components/testing";
 
 function App() {
   return (
     <>
       <div>
         <h1>Hello world</h1>
+        <Testing />
       </div>
     </>
   );
---
+++ src/components/testing.jsx
@@ -0,0 +1,11 @@
+
+
+const Testing = () => {
+  return (
+    <div>
+      <h1>Hello sahid this is abid from compo</h1>
+    </div>
+  );
+};
+
+export default Testing;
